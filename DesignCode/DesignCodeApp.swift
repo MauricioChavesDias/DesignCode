922b8306d6b022ee2e64804d2e7aa9b20d75e235
MauricioDias <mauriciodyas@gmail.com>
2021-10-25 12:57:17 +1100

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Mauricio Dias on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
