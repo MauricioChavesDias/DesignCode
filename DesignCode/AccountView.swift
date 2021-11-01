543631320eb6898ad54b0ffc4cf2eb83369cb9ba
MauricioDias <mauriciodyas@gmail.com>
2021-11-01 15:28:35 +1100

    Lecture 11: Structure and Toggles added.

--- DesignCode/AccountView.swift
+++ DesignCode/AccountView.swift
@@ -9,91 +9,110 @@ import SwiftUI
 
 struct AccountView: View {
     @State var isDeleted = false
+    @State var isPinned = false
     
     var body: some View {
         NavigationView {
             List {
-                //MARK: - Header
-                VStack {
-                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
-                        .symbolVariant(.circle.fill)
-                        .font(.system(size: 32))
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background(Circle().fill(.ultraThinMaterial))
-                        .background(
-                            Image(systemName: "hexagon")
-                                .symbolVariant(.fill)
-                                .foregroundColor(.blue)
-                                .font(.system(size: 200))
-                                .offset(x: -50, y: -100)
-                    )
-                    Text("Mauricio Chaves Dias")
-                        .font(.title.weight(.semibold))
-                    HStack {
-                        Image(systemName: "location")
-                            .imageScale(.small)
-                            .opacity(0.2)
-                        Text("Australia")
-                            .foregroundColor(.secondary)
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .padding()
-                
-                //MARK: - Sections: Settings - Billing - Help
-                Section {
-                    NavigationLink {ContentView()} label: {
-                        Label("Settings", systemImage: "gear")
-                    }
-                    NavigationLink { ContentView()} label: {
-                        Label("Billing", systemImage: "creditcard")
-                    }
-                    NavigationLink { ContentView()} label: {
-                        Label("Help", systemImage: "questionmark")
-                    }
-                }
-                .accentColor(.primary)
-                .listRowSeparatorTint(.blue)
-                .listRowSeparator(.hidden)
- 
-                //MARK: - Sections: Website, Youtube...
-                Section {
-                    if !isDeleted {
-                        Link(destination: URL(string: "https://apple.com")!) {
-                            HStack {
-                                Label("Website", systemImage: "house")
-                                Spacer()
-                                Image(systemName: "link")
-                                    .foregroundColor(.secondary)
-                            }
-                        }.swipeActions(edge: .leading, allowsFullSwipe: true){
-                            Button(action: { isDeleted = true }) {
-                                Label("Delete", systemImage: "trash")
-                            }.tint(.red)
-                            Button {} label: {
-                                Label("Pin", systemImage: "pin")
-                            }
-                            .tint(.yellow)
-                            
-                        }
-                    }
-                    
-                    Link(destination: URL(string: "https://youtube.com")!) {
-                        HStack {
-                            Label("Youtube", systemImage: "tv")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                }
-                .accentColor(.primary)
+                profile
+                sections
+                links
             }
             .listStyle(.insetGrouped)
             .navigationTitle("Account")
         }
+    }
+
+    var profile: some View {
+        VStack {
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                .symbolVariant(.circle.fill)
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(
+                    Image(systemName: "hexagon")
+                        .symbolVariant(.fill)
+                        .foregroundColor(.blue)
+                        .font(.system(size: 200))
+                        .offset(x: -50, y: -100)
+            )
+            Text("Mauricio Chaves Dias")
+                .font(.title.weight(.semibold))
+            HStack {
+                Image(systemName: "location")
+                    .imageScale(.small)
+                    .opacity(0.2)
+                Text("Australia")
+                    .foregroundColor(.secondary)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+    
+    var sections: some View {
+        Section {
+            NavigationLink {ContentView()} label: {
+                Label("Settings", systemImage: "gear")
+            }
+            NavigationLink { ContentView()} label: {
+                Label("Billing", systemImage: "creditcard")
+            }
+            NavigationLink { ContentView()} label: {
+                Label("Help", systemImage: "questionmark")
+            }
+        }
+        .accentColor(.primary)
+        .listRowSeparatorTint(.blue)
+        .listRowSeparator(.hidden)
+    }
+    
+    var links: some View {
+        Section {
+            if !isDeleted {
+                Link(destination: URL(string: "https://apple.com")!) {
+                    HStack {
+                        Label("Website", systemImage: "house")
+                        Spacer()
+                        Image(systemName: "link")
+                            .foregroundColor(.secondary)
+                    }
+                }.swipeActions(edge: .leading, allowsFullSwipe: true){
+                    deleteButtonWebsite
+                    pinButtonWebsite
+                }
+            }
+            
+            Link(destination: URL(string: "https://youtube.com")!) {
+                HStack {
+                    Label("Youtube", systemImage: "tv")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
+        .accentColor(.primary)
+    }
+    
+    var pinButtonWebsite: some View {
+        Button {isPinned.toggle()} label: {
+            if isPinned {
+                Label("Unpin", systemImage: "pin.slash")
+            } else {
+                Label("Pin", systemImage: "pin")
+            }
+        }
+        .tint(isPinned ? .gray : .yellow)
+    }
+
+    var deleteButtonWebsite: some View {
+        Button {isDeleted.toggle()} label: {
+            Label("Delete", systemImage: "trash")
+        }.tint(.red)
     }
 }
 
